0fbe638c8993df49adb567a04ba84405be14f5f7
Andre Victor <andre.leonidas458@gmail.com>
2025-03-04 23:43:47 -0300

    creating method for adding elements in the vector

--- src/com/victor/estruturadados/vetor/teste/Aula03.java
+++ src/com/victor/estruturadados/vetor/teste/Aula03.java
@@ -1,9 +1,22 @@
 package com.victor.estruturadados.vetor.teste;
 
+import com.victor.estruturadados.vetor.Vetor;
+
 public class Aula03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Vetor vetor = new Vetor(2);
+		
+		try {
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("elemento 2");
+			vetor.adiciona("elemento 3");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
